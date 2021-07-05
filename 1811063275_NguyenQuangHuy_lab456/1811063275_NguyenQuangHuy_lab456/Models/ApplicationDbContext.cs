a7757f6813233182d52cc426866f680f342bfc84
0358561452 <75628917+0358561452@users.noreply.github.com>
2021-07-05 19:35:51 +0700

    Course

---
+++ 1811063275_NguyenQuangHuy_lab456/1811063275_NguyenQuangHuy_lab456/Models/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+
+namespace _1811063275_NguyenQuangHuy_lab456.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public DbSet<Course> Course { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
